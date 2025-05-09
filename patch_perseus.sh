38e114ee94eb1c0904f6aa300de3c764cc232520
Firefly <fireflych@users.noreply.github.com>
2025-05-09 09:22:24 +0800

    Update patch_perseus.sh

--- patch_perseus.sh
+++ patch_perseus.sh
@@ -29,7 +29,7 @@ if [ ! -f "com.bilibili.AzurLane.apk" ]; then
 
     # eg: wget "your download link" -O "your packge name.apk" -q
     #if you want to patch .xapk, change the suffix here to wget "your download link" -O "your packge name.xapk" -q
-    wget https://github.com/fireflych/AzurLaneBiliBili-Perseus/releases/download/1.0/1.0.apk -O com.bilibili.AzurLane.apk --show-progress
+    wget https://github.com/fireflych/AzurLaneBiliBili-Perseus/releases/download/1.0/1.0.apk -O com.bilibili.AzurLane.apk -q
     echo "apk downloaded !"
     
     # if you can only download .xapk file uncomment 2 lines below. (delete the '#')
