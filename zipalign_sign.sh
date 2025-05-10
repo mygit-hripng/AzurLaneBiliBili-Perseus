5ceb64ad46457164ec8d96440d3e59c72114af4c
mygit-hripng <76903279+mygit-hripng@users.noreply.github.com>
2025-05-10 18:45:01 +0800

    Update zipalign_sign.sh

--- zipalign_sign.sh
+++ zipalign_sign.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-PATH=$PATH:$ANDROID_HOME/build-tools/32.0.0/
+# 调试：输出 Android SDK 路径
+echo "ANDROID_HOME: $ANDROID_HOME"
+# 调试：检查 zipalign 是否存在
+ls -l $ANDROID_HOME/build-tools/32.0.0/zipalign
 
+PATH=$PATH:$ANDROID_HOME/build-tools/32.0.0/
 for f in build/*.apk; do
     mv $f ${f%.apk}.apk.unsigned
     echo "Zipaligning $f"
